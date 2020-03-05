d5062a87d1db0803bde39fbdd514ad4a8f0d2130
Brandon Gerbasi <brandon.gerbasi@gmail.com>
2020-03-05 02:05:28 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(Phrase="Squawk!")
-  puts phrase
-  "phrase"
-  return "Phrase"
+def parrot(name="Squawk!")
+  puts name
+  return "name"
   end
   
